7f23ed4e3d40ae1049de1646379e1921c00feb26
yi3.zhang@intel.com <yi3.zhang@intel.com>
2024-06-17 16:25:29 +0800

    check sdpa fusion pattern

--- model_analysis.py
+++ model_analysis.py
@@ -1,8 +1,6 @@
-from transformers import AutoTokenizer, AutoModel, AutoConfig
-import torch
 import sys
 import numpy as np
-from openvino.runtime import Core, get_version, serialize
+from openvino.runtime import Core, get_version, serialize, Node
 import openvino as ov
 
 if len(sys.argv) < 2:
@@ -17,33 +15,48 @@ seq_len = 5
 input_ids = np.arange(batch_size*seq_len).reshape([batch_size, seq_len])
 attention_mask = np.ones([batch_size, seq_len])
 
-compiled_model = core.compile_model(ov_model, "CPU", {"PERFORMANCE_HINT": "LATENCY", "NUM_STREAMS": "1"})
-
 input_names = [output.get_node().get_friendly_name() for output in ov_model.inputs]
-
-print("model inputs {0}".format(input_names))
-
 original_inputs = {"input_ids": input_ids, "attention_mask": attention_mask, "position_ids": np.array(np.arange(seq_len).reshape([1, seq_len]) * batch_size), "beam_idx": np.zeros([batch_size])}
+print("model inputs {0}".format(input_names))
 
 actual_inputs = {}
 for name in input_names:
     if name in original_inputs:
         actual_inputs[name] = original_inputs[name]
     else:
-        print("get unexpected input {0}".format(name))
+        print("get unexpected input `{0}`, please check the model".format(name))
+        exit(-1)
 
+compiled_model = core.compile_model(ov_model, "CPU", {"PERFORMANCE_HINT": "LATENCY", "NUM_STREAMS": "1"})
 
 ov_results = compiled_model(actual_inputs)
+
+def check_sdpa(sdpa: Node):
+    # simple sdpa pattern check. only counts the memoryoutput nodes.
+    sdpa_outputs = sdpa.outputs()
+    if len(sdpa_outputs) == 1:
+        return False
+    output_type = {}
+    for output in sdpa_outputs:
+        child_nodes = output.get_target_inputs()
+        node = next(iter(child_nodes)).get_node()
+        rt_info = node.get_rt_info()
+        node_type = rt_info['layerType'].astype(str).lower()
+        output_type[node_type] = output_type[node_type] + 1 if node_type in output_type else 1
+
+    return False if 'memoryoutput' not in output_type else True
+        
         
 runtime_model = compiled_model.get_runtime_model()
-find_rope = False
+fuse_rope = False
+fuse_sdpa = False
 for execution_node in runtime_model.get_ordered_ops():
     rt_info = execution_node.get_rt_info()
     layer_type = rt_info['layerType'].astype(str)
     if layer_type.lower() == 'rope':
-        find_rope = True
-        break
-if find_rope:
-    print(":)Good Job Find Rope")
-else:
-    print("Bad Job Rope Not found")
+        fuse_rope = True
+    elif layer_type.lower() == 'scaleddotproductattention':
+        fuse_sdpa = check_sdpa(execution_node)
+        
+
+print("fuse rope {0}, fuse sdpa {1}".format(fuse_rope, fuse_sdpa))
